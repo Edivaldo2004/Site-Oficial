3b68328768304108fa80a8143d27d6a82ff0288c
Edivaldo Junior <edivaldosantosjunior2004@gmail.com>
2022-08-05 16:06:38 -0400

    Primeiro Commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-$('.carousel').carousel()
+let contador = 1;
+
+setInterval( function(){
+    document.getElementById('slide' + contador).checked = true;
+    contador++;
+
+    if(counter > 5 ) {
+        contador = 1;
+    }
+}, 3000 );
+ 
+pnload = start;
+
+function start(){
+    var i = 1;
+    function Move(){
+        i = (i%4)+1;
+        document.getElementById('t' +i).checked = true;
+    }
+    setInterval(Move, 7000);
+}
